3cafbc174d49b52ac73a46a4d6b4ce1569eb4a30
Mariana Caradec <maricaradec@gmail.com>
2024-05-25 17:44:59 -0300

    arreglo de estilos y funcionamiento del ingreso

--- js/script.js
+++ js/script.js
@@ -8,7 +8,9 @@ const cursos = [
         imgClass: 'img-curso',
         imgAlt: 'libera estres',
         descripcion: 'Es crucial liberar el estrés para mantener un equilibrio físico, mental y emocional. El estrés crónico puede tener efectos perjudiciales en nuestra salud, desde problemas físicos como dolores de cabeza y problemas digestivos, hasta consecuencias emocionales como la ansiedad y la depresión. Al liberar el estrés, podemos mejorar nuestra calidad de vida, fortalecer nuestro sistema inmunológico y promover un estado mental más positivo. Además, reduce la probabilidad de enfermedades relacionadas con el estrés, como enfermedades cardíacas y trastornos del sueño.',
+        descripcionClass: 'descripcion-curso',
         precio: 'Precio: $5000',
+        precioClass: 'precio-curso',
         comprado: false
     },
     {
@@ -17,7 +19,9 @@ const cursos = [
         imgClass: 'img-curso',
         imgAlt: 'mejorar el sueño',
         descripcion: 'Mejorar la calidad del sueño es crucial para nuestra salud física, mental y emocional. Un buen sueño promueve la salud física al reparar el cuerpo y fortalecer el sistema inmunológico, además de regular el metabolismo. A nivel mental, el sueño adecuado mejora la función cognitiva, la memoria y la concentración. También influye en nuestro estado de ánimo, reduciendo la irritabilidad y la ansiedad, y mejorando la salud mental en general. Además, el sueño adecuado facilita la recuperación muscular y del ejercicio, fortalece el sistema inmunológico y nos ayuda a mantenernos sanos y resistentes a las enfermedades.',
+        descripcionClass: 'descripcion-curso',
         precio: 'Precio: $7000',
+        precioClass: 'precio-curso',
         comprado: false
     },
     {
@@ -26,7 +30,9 @@ const cursos = [
         imgClass: 'img-curso',
         imgAlt: 'experimentar emociones',
         descripcion: 'Al experimentar conscientemente nuestras emociones, podemos aprender a manejar el estrés, mejorar nuestras relaciones interpersonales y aumentar nuestra autoconciencia. A través de la práctica de la atención plena y la autoexploración emocional, podemos cultivar una mayor comprensión y aceptación de nosotros mismos, lo que nos permite vivir de manera más auténtica y satisfactoria.',
+        descripcionClass: 'descripcion-curso',
         precio: 'Precio: $10000',
+        precioClass: 'precio-curso',
         comprado: false
     },
     {
@@ -35,7 +41,9 @@ const cursos = [
         imgClass: 'img-curso',
         imgAlt: 'sanar',
         descripcion: 'Al embarcarnos en este viaje de sanación, podemos sanar nuestras relaciones, fortalecer nuestra autoestima y liberarnos de patrones negativos que nos limitan. La sanación espiritual nos conecta con nuestro ser más profundo y nos ayuda a encontrar un propósito más elevado en la vida. A medida que nos sanamos emocionalmente y espiritualmente, podemos experimentar una mayor alegría, satisfacción y plenitud en todas las áreas de nuestra vida.',
+        descripcionClass: 'descripcion-curso',
         precio: 'Precio: $12000',
+        precioClass: 'precio-curso',
         comprado: false
     }
 ]
@@ -47,8 +55,8 @@ const crearMain = () => {
         section.innerHTML = `
                             <h3>${curso.titulo}</h3>
                             <img class= '${curso.imgClass}'src= ${curso.img} alt= ${curso.imgAlt}>
-                            <p>${curso.descripcion}</p>
-                            <p>${curso.precio}</p>
+                            <p class='${curso.descripcionClass}'>${curso.descripcion}</p>
+                            <p class= '${curso.precioClass}'>${curso.precio}</p>
                             <button id= 'boton-compra'>Comprar</button>
                             `
 
@@ -79,14 +87,11 @@ const crearMainNoExpertos = () => {
     link.className = 'link-curso';
     section.innerHTML = `
                                 <h3>Meditaciones guiadas - Nivel: principiante</h3>
-                                <a class= "link-curso">Visita la playlist (¡es gratuita!): ${link.src}</a>
+                                <a href='${link.href}' class= "link-curso" target="_blank">Visita la playlist (¡es gratuita!)</a>
                             `;
 
     mainNoExpertos.append(textoNoExpertos, section)
 }
-
-// function compra(comprar)
-// } 
 
 let datos = JSON.parse(localStorage.getItem('datos')) || [];
 
@@ -113,7 +118,6 @@ function iniciar() {
         const passwordIngresado = document.getElementById('password');
         const experienciaIngresada = document.getElementById('experiencia');
         ingresarDatos(userIngresado.value, passwordIngresado.value, experienciaIngresada.checked);
-        // mostrarDatos();
 
         if(experienciaIngresada.checked){
             main.style.display = 'block';
@@ -124,10 +128,10 @@ function iniciar() {
             mainNoExpertos.style.display = 'block';
             crearMainNoExpertos();
         }
-    })
 
-    form.reset();
+        const submit = document.getElementById('submit');
+        submit.disabled = true
+    })
 }
 
 iniciar()
-
