1f6c187ffafcad7ea01823e08081f67f277c45e3
Mariana Caradec <maricaradec@gmail.com>
2024-06-06 23:24:51 -0300

    uso de async await y try catch

--- js/script.js
+++ js/script.js
@@ -15,11 +15,61 @@ const darBienvenida = () => {
 };
 darBienvenida();
 
-// let main = document.getElementById("main");
+let main = document.getElementById("main");
 let mainNoExpertos = document.getElementById("main-no-expertos");
 
+const fetchCursos = async () => {
+    try {
+        const cursos = await fetch("./js/cursos.json")
+        const data = await cursos.json()
+        crearMain(data)
+    } catch (err) {
+        const error = Swal.fire({
+            icon: "error",
+            position: "top-end",
+            toast: true,
+            title: "Los cursos no se han cargado correctamente",
+            showConfirmButton: false,
+            timer: 2000
+        });
+    }
+}
 
-import { crearMain } from './cursos.mjs';
+fetchCursos()
+
+const crearMain = (cursos) => {
+    if (cursos && cursos.length > 0) {
+    cursos.forEach(curso => {
+        const section = document.createElement("div");
+        section.className = "curso";
+        section.innerHTML = `
+                                <h3>${curso.titulo}</h3>
+                                <img class= '${curso.imgClass}'src= ${curso.img} alt= ${curso.imgAlt}>
+                                <p class='${curso.descripcionClass}'>${curso.descripcion}</p>
+                                <p class= '${curso.precioClass}'>${curso.precio}</p>
+                                <button id= 'boton-compra'>Comprar</button>
+                                `;
+
+    const comprar = section.querySelector("#boton-compra");
+    comprar.addEventListener("click", (curso) => {
+        if (!curso.comprado) {
+        curso.comprado = true;
+        comprar.disabled = true;
+        const mensaje = Swal.fire({
+            icon: "success",
+            position: "top-end",
+            toast: true,
+            title: "El curso ha sido adquirido",
+            showConfirmButton: false,
+            timer: 2000
+        });
+        main.append(mensaje)
+        }
+    });
+    main.append(section);
+    });
+    }
+}
 
 const crearMainNoExpertos = () => {
     const textoNoExpertos = document.createElement("h2");
